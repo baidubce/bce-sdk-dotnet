fc8043454dabf0fe3aafa62153a33100608f33ff
lizhibiao <lizhibiao@e6699648-7b5c-4747-a937-56c23ec9ebad>
2015-12-02 13:28:26 +0000

    modify build.bat
    git-svn-id: https://svn.baidu.com/inf/bce/trunk/sdk/dotnet@97715 e6699648-7b5c-4747-a937-56c23ec9ebad

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 @echo off
 
 echo == %DATE% %TIME% ==
-set BUILD_HOME=%cd%
 set OutputFolderBase=output
 
 if exist %OutputFolderBase% (
@@ -15,13 +14,11 @@ echo == %DATE% %TIME% ==
 set PATH=D:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE;%PATH%
 
 :: compile release
-if %BUILD_RELEASE% equ 1 (
-    echo == %DATE% %TIME% ==
-    echo Compile Sources for Release...
-    msbuild BceSdkDotNet.sln /t:Clean;Rebuild /m:4 /p:Configuration=Release > x86.release.compile.log
-    xcopy /E /Y BceSdkDotNet\bin\2008\Release\*.* %OutputFolderBase%
-    echo ******************* Build Release Done! ********************
-)
+echo == %DATE% %TIME% ==
+echo Compile Sources for Release...
+msbuild BceSdkDotNet.sln /t:Clean;Rebuild /m:4 /p:Configuration=Release > x86.release.compile.log
+xcopy /E /Y BceSdkDotNet\bin\2008\Release\*.* %OutputFolderBase%
+echo ******************* Build Release Done! ********************
 
 @echo on
 exit
