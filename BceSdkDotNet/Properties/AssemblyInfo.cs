3f748b91cf98789a2c8194efd4a9cd48990cc96d
xuchuan <xuchuan@e6699648-7b5c-4747-a937-56c23ec9ebad>
2016-01-05 13:33:08 +0000

    Add nuspec file for NuGet package publication ISSUE=2692587
    git-svn-id: https://svn.baidu.com/inf/bce/trunk/sdk/dotnet@106305 e6699648-7b5c-4747-a937-56c23ec9ebad

--- BceSdkDotNet/Properties/AssemblyInfo.cs
+++ BceSdkDotNet/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("BceSdkDotNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BCE SDK for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Baidu.com, Inc.")]
 [assembly: AssemblyProduct("BceSdkDotNet")]
